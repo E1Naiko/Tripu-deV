38fee6331865b72e50e495f82c96e8d34c862080
Selgoku <99047802+Selgoku@users.noreply.github.com>
2023-09-27 00:51:30 -0300

    Update popup.js

--- La Cueva del Luchito/First-Extension/popup.js
+++ La Cueva del Luchito/First-Extension/popup.js
@@ -2,5 +2,5 @@ const message = document.querySelector('h1');
 const boton = document.querySelector('#cambiar-mensaje')
 
 boton.addEventListener('click', () => {
-    message.textContent = "https://www.youtube.com/watch?v=JxbMHSvI2DA <-- Aprendi acá :D"
+    message.textContent = "https://developer.chrome.com/docs/extensions/ <-- Aprendi acá :D"
 })
