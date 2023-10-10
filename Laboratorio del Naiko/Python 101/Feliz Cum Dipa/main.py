b633d2e471d89c1e92911b3cc3b54a108729dc9e
Nicolás Peñalba <matador50200@gmail.com>
2023-10-09 22:32:55 -0300

    Feliz cum dipa
    CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM

--- Laboratorio del Naiko/Python 101/Feliz Cum Dipa/main.py
+++ Laboratorio del Naiko/Python 101/Feliz Cum Dipa/main.py
@@ -1,2 +1,17 @@
-prt1 = lambda print("			|			|")
-FC = lambda print("FELIZ CUM")
+from PIL import Image
+from dibujitos import pintame, base1, base2, top1, top2
+
+cum = Image.open('cum.jpg')
+num = int(input ('ingrese el numero de añitos: '))
+
+for i in range(num):
+	top1()
+	top2()
+	bottom()
+	for j in range(num * 10):
+		pintame()
+	base1()
+	base2()
+	print()
+	
+cum.show()
